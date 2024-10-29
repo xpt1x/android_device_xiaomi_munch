91ec4668b0faa4f8c67e7567a4aed5b66470181a
lgjint <78463565+lgjint@users.noreply.github.com>
2024-10-29 19:29:03 +0800

    munch: Update blobs to V816.0.5.0.ULMMIXM
    Change-Id: I16eaf0a4520b874770a1057c5ab621fb4eca0ade

--- init/init_munch.cpp
+++ init/init_munch.cpp
@@ -32,7 +32,7 @@ static const variant_info_t munch_in_info = {
     .marketname = "POCO F4",
     .mod_device = "munch_in_global",
     .model = "22021211RI",
-    .build_fingerprint = "POCO/munch_in/munch:13/RKQ1.211001.001/V14.0.6.0.TLMMIXM:user/release-keys",
+    .build_fingerprint = "POCO/munch_in/munch:13/RKQ1.211001.001/V816.0.5.0.ULMMIXM:user/release-keys",
     .nfc = true,
 };
 
@@ -44,7 +44,7 @@ static const variant_info_t munch_global_info = {
     .marketname = "POCO F4",
     .mod_device = "munch_global",
     .model = "22021211RG",
-    .build_fingerprint = "POCO/munch_global/munch:13/RKQ1.211001.001/V14.0.6.0.TLMMIXM:user/release-keys",
+    .build_fingerprint = "POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.5.0.ULMMIXM:user/release-keys",
     .nfc = true,
 };
 
@@ -56,7 +56,7 @@ static const variant_info_t munch_info = {
     .marketname = "Redmi K40S",
     .mod_device = "munch_global",
     .model = "22021211RC",
-    .build_fingerprint = "Redmi/munch/munch:13/RKQ1.211001.001/V14.0.6.0.TLMMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/munch/munch:13/RKQ1.211001.001/V816.0.5.0.ULMMIXM:user/release-keys",
     .nfc = true,
 };
 
