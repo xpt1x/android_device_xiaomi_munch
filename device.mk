03d035f95b8c4fc657587645225a64dfe6410779
lgjint <78463565+lgjint@users.noreply.github.com>
2024-12-06 17:08:40 +0800

    munch: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- device.mk
+++ device.mk
@@ -98,6 +98,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libexif.vendor \
+    libgui.vendor \
+    liblz4.vendor \
+    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
@@ -297,6 +301,14 @@ PRODUCT_COPY_FILES += \
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
+# QMI
+PRODUCT_PACKAGES += \
+    libcurl.vendor \
+    libjson \
+    libjsoncpp.vendor \
+    libsqlite.vendor \
+    libvndfwk_detect_jni.qti
+
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
@@ -406,3 +418,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Wifi Display
+PRODUCT_PACKAGES += \
+    libpng.vendor
